9db865a1d97d280e402799ebd82bdf93f4db9fcc
therealspoljo <karlo.spoljar96@gmail.com>
2020-03-21 23:35:04 +0100

    Switched to named routes now

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,32 +3,40 @@
         <nav class="container mx-auto mb-20">
             <div class="px-8 py-6 flex items-center flex-col lg:flex-row text-center">
                 <div class="mb-10 lg:mb-0 lg:mr-8">
-                    <nuxt-link class="text-6xl font-bold" style="text-decoration:none!important;" to="/">4.a</nuxt-link>
+                    <nuxt-link
+                        class="text-6xl font-bold"
+                        style="text-decoration:none!important;"
+                        :to="{ name: 'index' }"
+                    >
+                        4.a
+                    </nuxt-link>
                 </div>
                 <div class="w-full lg:w-auto lg:ml-auto">
                     <ul
                         class="lg:flex items-center flex-col lg:flex-row text-lg lg:text-base font-semibold text-gray-500"
                     >
                         <li class="mb-2 lg:mb-0 lg:mr-4">
-                            <nuxt-link class="block p-2" to="/">Početna</nuxt-link>
+                            <nuxt-link class="block p-2" :to="{ name: 'index' }">Početna</nuxt-link>
                         </li>
                         <li class="mb-2 lg:mb-0 lg:mr-4">
-                            <nuxt-link class="block p-2" to="/hrvatski-jezik">Hrvatski jezik</nuxt-link>
+                            <nuxt-link class="block p-2" :to="{ name: 'hrvatski-jezik' }">Hrvatski jezik</nuxt-link>
                         </li>
                         <li class="mb-2 lg:mb-0 lg:mr-4">
-                            <nuxt-link class="block p-2" to="/matematika">Matematika</nuxt-link>
+                            <nuxt-link class="block p-2" :to="{ name: 'matematika' }">Matematika</nuxt-link>
                         </li>
                         <li class="mb-2 lg:mb-0 lg:mr-4">
-                            <nuxt-link class="block p-2" to="/priroda-i-drustvo">Priroda i društvo</nuxt-link>
+                            <nuxt-link class="block p-2" :to="{ name: 'priroda-i-drustvo' }">
+                                Priroda i društvo
+                            </nuxt-link>
                         </li>
                         <li class="mb-2 lg:mb-0 lg:mr-4">
-                            <nuxt-link class="block p-2" to="/likovna-kultura">Likovna kultura</nuxt-link>
+                            <nuxt-link class="block p-2" :to="{ name: 'likovna-kultura' }">Likovna kultura</nuxt-link>
                         </li>
                         <li class="mb-2 lg:mb-0 lg:mr-4">
-                            <nuxt-link class="block p-2" to="/glazbena-kultura">Glazbena kultura</nuxt-link>
+                            <nuxt-link class="block p-2" :to="{ name: 'glazbena-kultura' }">Glazbena kultura</nuxt-link>
                         </li>
                         <li>
-                            <nuxt-link class="block p-2" to="/tzk">TZK</nuxt-link>
+                            <nuxt-link class="block p-2" :to="{ name: 'tzk' }">TZK</nuxt-link>
                         </li>
                     </ul>
                 </div>
@@ -39,7 +47,10 @@
 
         <footer class="mt-32 mb-10 container mx-auto">
             <div class="px-8 py-6">
-                <p>Posljednje ažuriranje: {{ lastUpdated }}</p>
+                <p>
+                    Posljednje ažuriranje:
+                    {{ 'test' }}
+                </p>
             </div>
         </footer>
 
@@ -48,14 +59,17 @@
 </template>
 
 <script>
-import moment from 'moment'
+import { mapMutations } from 'vuex'
 
 export default {
     computed: {
-        lastUpdated() {
-            moment().locale('hr')
-            return moment(new Date()).format('Do MMM YYYY, H:mm:ss')
+        lastCommit() {
+            return this.$moment(new Date(this.$store.state.lastCommit)).format('Do MMMM YYYY, H:mm:ss')
         },
+    },
+
+    mounted() {
+        console.log(this.lastCommit)
     },
 }
 </script>
